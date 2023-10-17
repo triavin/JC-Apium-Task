469dd4ea411968616ae7818eb133f2c3a8c2d8a6
Aldi Triavin <aldi3avin@gmail.com>
2023-10-17 11:20:38 +0700

    add Utils

---
+++ src/main/java/com/juaracoding/utils/AddTransaksiElement.java
@@ -0,0 +1,13 @@
+package com.juaracoding.utils;
+
+public class AddTransaksiElement {
+    public static final String ADDBUTTON = "com.chad.financialrecord:id/fabMenu";
+    public static final String EXPENSE = "com.chad.financialrecord:id/btnExpense";
+    public static final String INCOME = "com.chad.financialrecord:id/btnIncome";
+    public static final String AMOUNT = "com.chad.financialrecord:id/etAmount";
+    public static final String NOTE = "com.chad.financialrecord:id/etNote";
+    public static final String CATEGORY = "com.chad.financialrecord:id/spCategory";
+    public static final String SAVE = "com.chad.financialrecord:id/btSave";
+
+
+}
---
+++ src/main/java/com/juaracoding/utils/IncomeCategory.java
@@ -0,0 +1,5 @@
+package com.juaracoding.utils;
+
+public class IncomeCategory {
+
+}
